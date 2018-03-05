ed49c463f832b1e06b71e1ab26f2c00be3708d74
qing <18814129853@163.com>
2018-03-05 18:58:54 +0800

    add

---
+++ src/com/reptile/util/DownLoadFile.java
@@ -0,0 +1,81 @@
+package com.reptile.util;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.params.BasicHttpParams;
+
+import java.io.*;
+
+public class DownLoadFile {
+
+    /**
+     * 根据URL和网页类型生成需要保存的网页的文件名， 去除URL中的非中文名字符
+     */
+    public String getFileNameByUrl (String url, String contentType) {
+        //移除http
+        url = url.substring(7);
+        //text/html 类型
+        if (contentType.indexOf("html") != -1) {
+            url = url.replaceAll("[\\?/:*|<>\"]", "_") + ".html";
+            return url;
+        }else {
+            return url.replace("[\\?/:*|<>\"]", "_") + "." + contentType.substring(contentType.lastIndexOf("/")+1);
+        }
+    }
+
+    /**
+     * 保存网页字节数组到本地文件， filePath为保存的文件的相对地址
+     */
+    private void saveToLocal (byte[] data, String filePath) {
+        try {
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(new File(filePath)));
+            for (int i = 0; i < data.length; i++) {
+                out.write(data[i]);
+            }
+            out.flush();
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 下载URL指向的网页
+     *
+     */
+    public String downloadFile (String url) {
+        String filePath = null;
+        //1.生成HttpClient 对象并设置参数
+        HttpClient httpClient = HttpClients.createDefault();
+        //设置HTTP连接超时5s
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(5000).setSocketTimeout(5000).build();
+        //生成GetMethod对象并设置参数
+        HttpGet httpGet = new HttpGet(url);
+        //设置请求重试处理
+
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            //判断访问的状态码
+            if (httpResponse.getStatusLine().getStatusCode() != 200) {
+                System.err.println("Method failed : " + httpResponse.getStatusLine());
+                filePath = null;
+            }
+            //处理HTTP响应的内容
+            InputStream content = httpResponse.getEntity().getContent();
+
+            //根据网页URL生成保存时的文件名
+            filePath = "temp\\" + getFileNameByUrl(url, httpResponse.getEntity().getContentType().getValue());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/com/reptile/util/Queue.java
@@ -0,0 +1,35 @@
+package com.reptile.util;
+
+import java.util.LinkedList;
+
+/**
+ * 队列， 保存将要访问的URL
+ */
+public class Queue {
+    //使用链表实现队列
+    private LinkedList queue = new LinkedList();
+    //入队列
+    public void enQueue (Object o) {
+        queue.addLast(o);
+
+    }
+
+    //出队列
+    public Object deQueue () {
+        return queue.removeFirst();
+    }
+
+    //判断队列是否为空
+    public boolean isQueueEmpty () {
+        return queue.isEmpty();
+    }
+
+    //判断队列是否包含某个
+    public boolean contians (Object o) {
+        return queue.contains(o);
+    }
+
+    public boolean empty () {
+        return queue.isEmpty();
+    }
+}
